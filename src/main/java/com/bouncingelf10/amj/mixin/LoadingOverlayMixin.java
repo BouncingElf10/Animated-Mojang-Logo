d05828ae56f334e7d57052b51bd8f0e293b824ef
BouncingElf10 <168445249+BouncingElf10@users.noreply.github.com>
2026-08-28 20:50:44 +0200

    Merge pull request #29 from tizianoluziramos/26.1-fabric
    Preloads all animation frames

--- src/main/java/com/bouncingelf10/amj/mixin/LoadingOverlayMixin.java
+++ src/main/java/com/bouncingelf10/amj/mixin/LoadingOverlayMixin.java
@@ -34,6 +34,7 @@ public class LoadingOverlayMixin {
 
 		tickFadeIn(self, now);
 		smoothProgress(self);
+		MojangAnimFrameManager.tickPreload();
 
 		float fadeOut = getFadeOutProgress(self, now);
 		float fadeIn = getFadeInProgress(self, now);
@@ -41,7 +42,7 @@ public class LoadingOverlayMixin {
 		guiGraphics.fill(0, 0, guiGraphics.guiWidth(), guiGraphics.guiHeight(), ColorManager.getBackground());
 		renderProgressBar(guiGraphics, self, fadeOut);
 
-		if (fadeOut >= 2.0F && AnimatedMojangLogoClient.isInit) {
+		if (fadeOut >= 2.0F && AnimatedMojangLogoClient.isInit && MojangAnimFrameManager.areFramesPreloaded()) {
 			renderMojangAnim(guiGraphics);
 			if (MojangAnimFrameManager.hasFinished) removeOverlay(self);
 		}
