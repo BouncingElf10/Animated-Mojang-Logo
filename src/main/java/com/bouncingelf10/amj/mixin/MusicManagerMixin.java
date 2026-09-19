e75154e7c9f430dcf81dbc68f43f5c92d89204c7
BouncingElf10 <168445249+BouncingElf10@users.noreply.github.com>
2026-09-19 16:15:54 +0200

    prevented music playback during logo animation

---
+++ src/main/java/com/bouncingelf10/amj/mixin/MusicManagerMixin.java
@@ -0,0 +1,23 @@
+package com.bouncingelf10.amj.mixin;
+
+import com.bouncingelf10.amj.AnimatedMojangLogoClient;
+import com.bouncingelf10.amj.config.ModConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.LoadingOverlay;
+import net.minecraft.client.sounds.MusicManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MusicManager.class)
+public class MusicManagerMixin {
+
+	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+	private void amj$holdDuringAnimation(CallbackInfo ci) {
+		if (Minecraft.getInstance().getOverlay() instanceof LoadingOverlay
+				&& ModConfig.isEnabled()
+				&& !(AnimatedMojangLogoClient.hasRunOnce && ModConfig.shouldOnlyPlayOnce()))
+			ci.cancel();
+	}
+}
